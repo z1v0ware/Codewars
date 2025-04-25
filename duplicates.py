b8b8cb3c315db02a37b6dcde344aeb98d05e6e9c
_David_ <zivo.cyber02@gmail.com>
2025-04-25 20:28:02 +0200

    Comments

--- duplicates.py
+++ duplicates.py
@@ -1,10 +1,10 @@
 # Remove duplicates from a list
 
 def distinct(seq):
-    no_duplicates = []
-    for i in seq:
-        if i not in no_duplicates:
+    no_duplicates = []  # Create an empty list
+    for i in seq:  # Iterate the given list
+        if i not in no_duplicates:  # Make sure to append elements that aren't already in our list
             no_duplicates.append(i)
-        else:
+        else:  # If the element is in the list we keep going
             continue
-    return no_duplicates
+    return no_duplicates  # Return the list without duplicates
