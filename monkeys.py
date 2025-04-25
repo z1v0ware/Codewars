9432ac70d9dcead018d68d3a5a2601fa4af1ea8d
_David_ <zivo.cyber02@gmail.com>
2025-04-25 20:31:27 +0200

    Comments

--- monkeys.py
+++ monkeys.py
@@ -1,5 +1,5 @@
 # Count the Monkeys! (8kyu)
 
 def monkey_count(n):
-    monkeys = [i+1 for i in range(n)]
-    return monkeys
+    monkeys = [i+1 for i in range(n)]  # Create a list with the numbers from 1 to n
+    return monkeys  # Return the new array
